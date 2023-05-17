c2b76e8a01f902de82d18c27bfc1c438840a026e
Sumsum Gogoi <sumsumgogoi51@gmail.com>
2023-05-17 16:31:12 +0530

    mmm

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -59,10 +59,15 @@ export const payemntVerification = catchAsyncError(async (req, res, next) => {
 
   user.subscription.status = "active";
   await user.save();
+  
+  setTimeout(()=>{
+    res.redirect(
+      `${process.env.FRONTEND_URL}/paymentsuccess?reference=${razorpay_payment_id}`
+    );
 
-  res.redirect(
-    `${process.env.FRONTEND_URL}/paymentsuccess?reference=${razorpay_payment_id}`
-  );
+  },2000)
+
+  
 });
 
 export const getRazorpayKey = catchAsyncError(async (req, res, next) => {
